fc87b6e3681d75290daf199b76da9172e6154e31
aleksey.krasnov <krasnov.80@mail.ru>
2023-04-18 14:14:50 +0300

    Task done

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,4 +7,27 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 Console.Clear();
+string[] arr={"Hello", "2", "world", ":-)"};
+int numberСhar=3; //длина которых меньше, либо равна 3 символам
+Console.WriteLine($"Массив: [{String.Join(", ",arr)}]");
+int count=0;
+for (int i=0; i<arr.Length;i++) //определяем размер нового массива
+{
+    if (arr[i].Length<=numberСhar)
+    {
+        count++;
+    }
+}
+
+string[] newArr=new string[count];
+count=0;
+for (int i=0; i<arr.Length;i++) //заносим данны в новый массив
+{
+    if (arr[i].Length<=numberСhar)
+    {
+        newArr[count]=arr[i];
+        count++;
+    }
+}
+Console.WriteLine($"Новый массив: [{String.Join(", ",newArr)}]");
 
